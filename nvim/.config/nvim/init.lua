2f9fbdbf6197889afa367dd3d5f607443f428057
Yaman Adamnor <yamix2000@gmail.com>
2023-06-27 12:59:39 +0200

    fix: Fixed slow copy and paste performance

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,19 +25,20 @@ end
 vim.o.fillchars = "eob: "
 ------------------------------------------
 -- Yank to clipboard on WSL
-vim.opt.clipboard:append({ "unnamed", "unnamedplus" }) 
-vim.g.clipboard =  {
-    name= 'WslClipboard',
-    copy= {
-        ['+'] = 'clip.exe',
-        ['*'] = 'clip.exe',
-    },
-    paste = {
-        ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-        ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-}
+vim.opt.clipboard = 'unnamedplus'
+
+-- vim.g.clipboard = {
+--     name = 'WslClipboard',
+--     copy = {
+--         ['+'] = 'clip.exe',
+--         ['*'] = 'clip.exe',
+--     },
+--     paste = {
+--         ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--         ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+--     },
+--     cache_enabled = 0,
+-- }
 ------------------------------------------
 -- VimTeX
 
