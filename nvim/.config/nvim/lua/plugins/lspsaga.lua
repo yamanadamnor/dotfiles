ba1fd691434730ce7aa9f1b811463168ef6987d7
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2023-12-10 17:05:56 +0100

    update lspsaga keymaps

--- nvim/.config/nvim/lua/plugins/lspsaga.lua
+++ nvim/.config/nvim/lua/plugins/lspsaga.lua
@@ -17,7 +17,7 @@ end
 
 local opts = { silent = true }
 
-vim.keymap.set("n", "<C-a>", "<cmd>Lspsaga code_action<CR>")
+vim.keymap.set("n", "<space>ca", "<cmd>Lspsaga code_action<CR>")
 vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>")
 vim.keymap.set("n", "gr", "<cmd>Lspsaga lsp_finder<CR>")
 vim.keymap.set("i", "<C-k>", "<cmd>Lspsaga signature_help<CR>")
