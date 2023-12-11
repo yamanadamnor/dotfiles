19e5104035391b2e17c15595f15d4abda7daf1b9
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2023-12-12 00:05:03 +0100

    cleanup

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,64 +1,65 @@
 return {
-    -- Various colorschemes
-    "flazz/vim-colorschemes",
-    "w0ng/vim-hybrid",
+	-- Various colorschemes
+	"flazz/vim-colorschemes",
+	"w0ng/vim-hybrid",
 
-    -- Colorscheme
-    {
-        "rebelot/kanagawa.nvim",
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            require("kanagawa").setup({
-                transparent = true,
-                theme = "dark",
-                overrides = function()
-                    return {
-                        TelescopeBorder = { default = true, link = "TelescopeNormal" },
-                        TelescopePromptBorder = { default = true, link = "TelescopeBorder" },
-                        TelescopeResultsBorder = { default = true, link = "TelescopeBorder" },
-                        TelescopePreviewBorder = { default = true, link = "TelescopeBorder" },
-                    }
-                end,
-            })
-            vim.cmd([[colorscheme kanagawa]])
-        end,
-    },
+	-- Colorscheme
+	{
+		"rebelot/kanagawa.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			require("kanagawa").setup({
+				transparent = true,
+				theme = "dark",
+				overrides = function()
+					return {
+						TelescopeBorder = { default = true, link = "TelescopeNormal" },
+						TelescopePromptBorder = { default = true, link = "TelescopeBorder" },
+						TelescopeResultsBorder = { default = true, link = "TelescopeBorder" },
+						TelescopePreviewBorder = { default = true, link = "TelescopeBorder" },
+					}
+				end,
+			})
+			vim.cmd([[colorscheme kanagawa]])
+		end,
+	},
 
-    -- Emmet
-    "mattn/emmet-vim",
+	-- Emmet
+	"mattn/emmet-vim",
 
-    -- Commenting
-    {
-        'numToStr/Comment.nvim',
-        config = function()
-            require("Comment").setup({
-                pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
-            })
-        end
-    },
+	-- Comment support for TSX support
+	{
 
-    -- Change surrounding symbols
-    "tpope/vim-surround",
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false,
+			})
+		end,
+	},
 
-    -- Detect indentation
-    "tpope/vim-sleuth",
+	-- Change surrounding symbols
+	"tpope/vim-surround",
 
-    -- Git stuff
-    "tpope/vim-fugitive",
+	-- Detect indentation
+	"tpope/vim-sleuth",
 
-    -- Highlight colors
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup({})
-        end,
-    },
+	-- Git stuff
+	"tpope/vim-fugitive",
 
-    -- VimTeX
-    "lervag/vimtex",
+	-- Highlight colors
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({})
+		end,
+	},
 
-    -- Caddyfile support
-    "isobit/vim-caddyfile"
+	-- VimTeX
+	"lervag/vimtex",
+
+	-- Caddyfile support
+	"isobit/vim-caddyfile",
 }
