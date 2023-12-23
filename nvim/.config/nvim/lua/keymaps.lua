4635f8ae2ac550330d5ac2cb2077a3a3883f2b5c
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2023-12-23 17:16:34 +0100

    nvim: Add TODO comments highlighting

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,6 +1,8 @@
 local keymap = vim.keymap
 local opts = { noremap, silent = true }
 
+keymap.set("n", "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>")
+
 -- Code actions
 keymap.set("n", "<space>ca", function()
 	vim.lsp.buf.code_action()
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -64,4 +64,15 @@ return {
 	"isobit/vim-caddyfile",
 
 	"dstein64/vim-startuptime",
+
+	-- TODO comments hightlighting
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 }
