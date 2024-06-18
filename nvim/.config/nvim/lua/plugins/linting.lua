825a73059578ee33c50dbcb4bdfb4f818cbb0256
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2024-06-18 14:36:30 +0200

    vim: Use eslint instead of eslint_d
    eslint_d caused issues

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -1,16 +1,16 @@
 local M = {
-    "mfussenegger/nvim-lint",
-    event = {"BufReadPre", "BufNewFile"}
+  "mfussenegger/nvim-lint",
+  event = { "BufReadPre", "BufNewFile" },
 }
 
 function M.config()
-    local lint = require("lint")
-    lint.linters_by_ft = {
-        javascript = {"eslint_d"},
-        typescript = {"eslint_d"},
-        javascriptreact = {"eslint_d"},
-        typescriptreact = {"eslint_d"}
-    }
+  local lint = require("lint")
+  lint.linters_by_ft = {
+    javascript = { "eslint" },
+    typescript = { "eslint" },
+    javascriptreact = { "eslint" },
+    typescriptreact = { "eslint" },
+  }
 end
 
 return M
