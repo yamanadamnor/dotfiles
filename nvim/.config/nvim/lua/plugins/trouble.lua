b1c18570884ce95da7b1e8d5985731173b90447a
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2024-06-18 14:34:27 +0200

    vim: Migrate to trouble v3

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,18 +1,37 @@
 local M = {
-    "folke/trouble.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
+  "folke/trouble.nvim",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  cmd = "Trouble",
 }
 
+function M.keys() end
+
 function M.config()
-    require("trouble").setup({})
+  require("trouble").setup({
+    modes = {
+      mydiags = {
+        mode = "diagnostics", -- inherit from diagnostics mode
+        filter = {
+          any = {
+            buf = 0, -- current buffer
+            {
+              severity = vim.diagnostic.severity.ERROR, -- errors only
+              -- limit to files in the current project
+              function(item)
+                return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
+              end,
+            },
+          },
+        },
+      },
+    },
+  })
 end
 
 local opts = { silent = true, noremap = true }
 
-vim.keymap.set("n", "<leader>xx", function() require("trouble").open() end, opts)
-vim.keymap.set("n", "<leader>xw", function() require("trouble").open("workspace_diagnostics") end, opts)
-vim.keymap.set("n", "<leader>xd", function() require("trouble").open("document_diagnostics") end, opts)
-vim.keymap.set("n", "<leader>xq", function() require("trouble").open("quickfix") end, opts)
-vim.keymap.set("n", "<leader>xl", function() require("trouble").open("loclist") end, opts)
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", opts)
+vim.keymap.set("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", opts)
+vim.keymap.set("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", opts)
 
 return M
