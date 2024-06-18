d0871b87cdce0a7801b26483e9eb27f882f3346e
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2024-06-18 14:37:04 +0200

    vim: Update cmp

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,100 +1,101 @@
 local M = {
-	-- Auto completion
-	"hrsh7th/nvim-cmp",
-	dependencies = {
-		"hrsh7th/cmp-buffer",
-		"hrsh7th/cmp-nvim-lsp",
-		"L3MON4D3/LuaSnip",
-		"onsails/lspkind.nvim",
+  -- Auto completion
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-buffer",
+    "hrsh7th/cmp-nvim-lsp",
+    "L3MON4D3/LuaSnip",
+    "onsails/lspkind.nvim",
 
-		-- collection of snippets
-		"rafamadriz/friendly-snippets",
+    -- collection of snippets
+    "rafamadriz/friendly-snippets",
 
-		-- LuaSnip completion source for nvim-cmp
-		"saadparwaiz1/cmp_luasnip",
+    -- LuaSnip completion source for nvim-cmp
+    "saadparwaiz1/cmp_luasnip",
 
-		-- Github Copilot suggestions as cmp sources
-		"zbirenbaum/copilot-cmp",
-	},
+    -- Github Copilot suggestions as cmp sources
+    "zbirenbaum/copilot-cmp",
+  },
 }
 
 function M.config()
-	local cmp = require("cmp")
-	local luasnip = require("luasnip")
-	local lspkind = require("lspkind")
-	local copilot_cmp = require("copilot_cmp")
+  local cmp = require("cmp")
+  local luasnip = require("luasnip")
+  local lspkind = require("lspkind")
+  local copilot_cmp = require("copilot_cmp")
 
-	local has_words_before = function()
-		if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-			return false
-		end
-		local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-		return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-	end
+  local has_words_before = function()
+    if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+      return false
+    end
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0
+      and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  end
 
-	copilot_cmp.setup()
+  copilot_cmp.setup()
 
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				luasnip.lsp_expand(args.body)
-			end,
-		},
-		mapping = {
-			["<C-d>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete(),
-			["<C-e>"] = cmp.mapping.close(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-			["<Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_next_item()
-				elseif luasnip.jumpable(1) then
-					-- Jump to the next node
-					luasnip.jump(1)
-				elseif has_words_before() then
-					cmp.complete()
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end,
+    },
+    mapping = {
+      ["<C-d>"] = cmp.mapping.scroll_docs(-4),
+      ["<C-f>"] = cmp.mapping.scroll_docs(4),
+      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-e>"] = cmp.mapping.close(),
+      ["<CR>"] = cmp.mapping.confirm({ select = true }),
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        elseif luasnip.jumpable(1) then
+          -- Jump to the next node
+          luasnip.jump(1)
+        elseif has_words_before() then
+          cmp.complete()
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
 
-			["<S-Tab>"] = cmp.mapping(function(fallback)
-				if cmp.visible() then
-					cmp.select_prev_item()
-				elseif luasnip.jumpable(-1) then
-					luasnip.jump(-1)
-				else
-					fallback()
-				end
-			end, { "i", "s" }),
-		},
-		formatting = {
-			format = lspkind.cmp_format({
-				mode = "symbol", -- show only symbol annotations
-				maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-				-- can also be a function to dynamically calculate max width such as
-				-- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-				ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-				show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+      ["<S-Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          fallback()
+        end
+      end, { "i", "s" }),
+    },
+    formatting = {
+      format = lspkind.cmp_format({
+        mode = "symbol", -- show only symbol annotations
+        maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+        -- can also be a function to dynamically calculate max width such as
+        -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+        ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+        show_labelDetails = true, -- show labelDetails in menu. Disabled by default
 
-				-- The function below will be called before any actual modifications from lspkind
-				-- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-				before = function(entry, vim_item)
-					return vim_item
-				end,
-			}),
-		},
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp", group_index = 1 },
-			{ name = "copilot", group_index = 2 },
-			{ name = "luasnip", group_index = 2 },
-		}, {
-			{ name = "buffer" },
-		}),
-	})
+        -- The function below will be called before any actual modifications from lspkind
+        -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+        before = function(entry, vim_item)
+          return vim_item
+        end,
+      }),
+    },
+    sources = cmp.config.sources({
+      { name = "nvim_lsp", group_index = 1 },
+      { name = "copilot", group_index = 2 },
+      { name = "luasnip", group_index = 2 },
+    }, {
+      { name = "buffer" },
+    }),
+  })
 
-	require("luasnip.loaders.from_vscode").lazy_load()
+  require("luasnip.loaders.from_vscode").lazy_load()
 end
 
 return M
