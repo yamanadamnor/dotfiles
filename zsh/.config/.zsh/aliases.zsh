079ad8f4d8c0358cf4da2c5c03f00fe6021ce2d1
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2024-06-25 14:32:55 +0200

    zsh: Add colorized output from --help flags

--- zsh/.config/.zsh/aliases.zsh
+++ zsh/.config/.zsh/aliases.zsh
@@ -12,3 +12,7 @@ alias kittyconfig="nvim $DOTFILES/kitty/.config/kitty/kitty.conf"
 alias vimconfig="cd $DOTFILES/nvim/.config/nvim && nvim $DOTFILES/nvim/.config/nvim/init.lua"
 alias zconfig="nvim $DOTFILES/zsh/.zshrc"
 alias getlab="python3 $HOME/Projects/getlab/staff_cli.py"
+
+# Colorize help output with bat
+alias -g -- -h='-h 2>&1 | bat --language=help --style=plain'
+alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
