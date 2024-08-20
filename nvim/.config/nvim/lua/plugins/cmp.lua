525e77e8d158a97c49383d86b73494d9f0121755
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2024-08-20 09:27:42 +0200

    nvim: Add Github Copilot cmp source symbol

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -78,6 +78,9 @@ function M.config()
         -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
         ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
         show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+        symbol_map = {
+          Copilot = "",
+        },
 
         -- The function below will be called before any actual modifications from lspkind
         -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
