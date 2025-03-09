2c7a256826c1ed6e55b12d771c59df3fac3eb413
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-03-09 18:50:08 +0100

    nvim: Add docker compose filetypes

--- nvim/.config/nvim/lua/filetypes.lua
+++ nvim/.config/nvim/lua/filetypes.lua
@@ -2,5 +2,21 @@ vim.filetype.add({
   pattern = {
     -- INFO: Match filenames like - ".env.example", ".env.local" and so on
     ["%.env%.[%w_.-]+"] = "sh",
+    ["docker-compose%.yml"] = "yaml.docker-compose",
+    ["docker-compose%.yaml"] = "yaml.docker-compose",
+    ["compose%.yml"] = "yaml.docker-compose",
+    ["compose%.yaml"] = "yaml.docker-compose",
+  },
+  filename = {
+    ["docker-compose-build.yml"] = "yaml.docker-compose",
+    ["docker-compose-build.yaml"] = "yaml.docker-compose",
+    ["docker-compose-prod.yml"] = "yaml.docker-compose",
+    ["docker-compose-prod.yaml"] = "yaml.docker-compose",
+    ["docker-compose-test.yml"] = "yaml.docker-compose",
+    ["docker-compose-test.yaml"] = "yaml.docker-compose",
+    ["docker-compose.yml"] = "yaml.docker-compose",
+    ["docker-compose.yaml"] = "yaml.docker-compose",
+    ["compose.yml"] = "yaml.docker-compose",
+    ["compose.yaml"] = "yaml.docker-compose",
   },
 })
