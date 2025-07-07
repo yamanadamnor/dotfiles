c2a2a186bcd5c6a563de8b474c415f5c5cde635f
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-07-07 15:56:49 +0200

    zsh: Update aliases

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -3,7 +3,7 @@ alias ..="cd .."
 alias ls="eza"
 alias ll="eza -la"
 alias lg="lazygit"
-alias synk="rsync -avhP --info=progress2" # archive, verbose, human, partial
+alias synk="rsync -s  --size-only -avhP --info=progress2" # archive, verbose, human, partial
 alias w++17="g++ -std=c++17 -Wall -Wextra -Wpedantic -Weffc++ -Wold-style-cast"
 alias checkports="sudo lsof -i -P | grep LISTEN | grep :$PORT"
 alias ssh="TERM=xterm ssh"
