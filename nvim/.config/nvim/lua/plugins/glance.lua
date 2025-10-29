9aee86d997b6598853e085e47a3df821f465a2db
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2025-10-29 14:08:35 +0100

    nvim: Add glance

---
+++ nvim/.config/nvim/lua/plugins/glance.lua
@@ -0,0 +1,26 @@
+return {
+  "dnlhc/glance.nvim",
+  cmd = "Glance",
+  keys = {
+    {
+      "gd",
+      "<cmd>Glance definitions<CR>",
+      desc = "LSP Definitions (Glance)",
+    },
+    {
+      "gr",
+      "<cmd>Glance references<CR>",
+      desc = "LSP References (Glance)",
+    },
+    {
+      "gi",
+      "<cmd>Glance implementations<CR>",
+      desc = "LSP Implementations (Glance)",
+    },
+    {
+      "gt",
+      "<cmd>Glance type_definitions<CR>",
+      desc = "LSP Type Definitions (Glance)",
+    },
+  },
+}
