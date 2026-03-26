b271485e4c7dca23334ba6ee3dc0715591da2c40
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-03-26 13:33:16 +0100

    nvim(WSL): This fixes the ^M issue in WSL/Windows Terminal

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -41,5 +41,19 @@ vim.o.fillchars = "eob: "
 -- Use system clipboard
 vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
 
+-- Use this on Windows when using WSL
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
+
 ------------------------------------------
 require("config")
