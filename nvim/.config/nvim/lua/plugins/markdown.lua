1ffb8b2ecf1fc5865bc56834eef5b87c4668251a
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-04-10 10:20:47 +0200

    nvim: Add lsp support to markdown

--- nvim/.config/nvim/lua/plugins/markdown.lua
+++ nvim/.config/nvim/lua/plugins/markdown.lua
@@ -1,8 +1,18 @@
 local M = {
   "MeanderingProgrammer/render-markdown.nvim",
   dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
-  opts = {},
+
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {
+    completions = {
+      lsp = {
+        enabled = true
+      }
+    }
+  },
   ft = "markdown",
 }
+
 
 return M
