65da9b8b075b8e694a98e8276fcc3cba680db45f
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-04-10 10:20:07 +0200

    nvim: Update trouble config

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -2,42 +2,20 @@ local M = {
   "folke/trouble.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
   cmd = "Trouble",
-}
-
-function M.keys() end
-
-function M.config()
-  require("trouble").setup({
-    modes = {
-      mydiags = {
-        mode = "diagnostics", -- inherit from diagnostics mode
-        filter = {
-          any = {
-            buf = 0,                                    -- current buffer
-            {
-              severity = vim.diagnostic.severity.ERROR, -- errors only
-              -- limit to files in the current project
-              function(item)
-                return item.filename:find((vim.loop or vim.uv).cwd(), 1, true)
-              end,
-            },
-          },
-        },
-      },
+  opts = {},
+  ---@type table<string, trouble.Action.spec|false>
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
     },
-    keys = {
-      {
-        "<leader>xx",
-        "<cmd>Trouble diagnostics toggle<cr>",
-        desc = "Diagnostics (Trouble)",
-      },
-      {
-        "<leader>ft",
-        "<cmd>Trouble todo keywords=TODO,FIX,FIXME<cr>",
-        desc = "TODOs (Trouble)",
-      }
+    {
+      "<leader>ft",
+      "<cmd>Trouble todo keywords=TODO,FIX,FIXME<cr>",
+      desc = "TODOs (Trouble)",
     }
-  })
-end
+  }
+}
 
 return M
