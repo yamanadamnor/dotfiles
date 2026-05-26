b0ddf18c52ac0492953f0521c6db30b90d02cc06
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-05-26 10:35:13 +0200

    nvim: Format files

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,11 +1,8 @@
 local M = {
-  "numToStr/Comment.nvim",
+    "numToStr/Comment.nvim",
+    opts = {
+        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+    }
 }
-
-function M.config()
-  require("Comment").setup({
-    pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-  })
-end
 
 return M
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,47 +1,42 @@
 return {
-  -- Colorscheme
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      require("catppuccin").setup({
-        flavor = "macchiato",
-        auto_integrations = true,
-      })
-      vim.cmd([[colorscheme catppuccin]])
-    end,
-  },
+    -- Colorscheme
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require("catppuccin").setup({
+                flavor = "macchiato",
+                auto_integrations = true,
+            })
+            vim.cmd([[colorscheme catppuccin]])
+        end,
+    },
 
-  -- Emmet
-  "mattn/emmet-vim",
+    -- Emmet
+    "mattn/emmet-vim",
 
-  -- Comment support for TSX support
-  {
+    -- Comment support for TSX support
+    {
 
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    config = function()
-      require("ts_context_commentstring").setup({
-        enable_autocmd = false,
-      })
-    end,
-  },
+        "JoosepAlviste/nvim-ts-context-commentstring",
+        config = function()
+            require("ts_context_commentstring").setup({
+                enable_autocmd = false,
+            })
+        end,
+    },
 
-  -- Change surrounding symbols
-  {
-    "tpope/vim-surround",
-    event = "VeryLazy",
-  },
+    -- Change surrounding symbols
+    {
+        "tpope/vim-surround",
+        event = "VeryLazy",
+    },
 
-  -- Detect indentation
-  "tpope/vim-sleuth",
-
-  -- Highlight colors
-  {
-    "catgoose/nvim-colorizer.lua",
-    event = "BufReadPre",
-    opts = {},
-  },
-  -- Caddyfile support
-  "isobit/vim-caddyfile",
+    -- Highlight colors
+    {
+        "catgoose/nvim-colorizer.lua",
+        event = "BufReadPre",
+        opts = {},
+    },
 }
