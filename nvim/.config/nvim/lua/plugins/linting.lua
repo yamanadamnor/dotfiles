cab62c7a6584cd8c7bea03f7e0b1b1ae3504158a
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-05-26 10:37:33 +0200

    nvim: Update linting config

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -1,12 +1,17 @@
 local M = {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
 }
 
 function M.config()
-  local lint = require("lint")
-  lint.linters_by_ft = {
-  }
+    local lint = require("lint")
+    lint.linters_by_ft = {
+        javascript = { "oxlint" },
+        typescript = { "oxlint" },
+        javascriptreact = { "oxlint" },
+        typescriptreact = { "oxlint" },
+        markdown = { "markdownlint" },
+    }
 end
 
 return M
