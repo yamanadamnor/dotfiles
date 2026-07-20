81edaf82f2bb11cdfe9c138f5f4b5d75662a16de
Yaman Adamnor <32465405+yamanadamnor@users.noreply.github.com>
2026-07-20 11:15:39 +0200

    nvim: add roslyn for C# support

---
+++ nvim/.config/nvim/lua/plugins/roslyn.lua
@@ -0,0 +1,11 @@
+local M = {
+    "seblyng/roslyn.nvim",
+    ---@module 'roslyn.config'
+    ---@type RoslynNvimConfig
+    opts = {
+        -- your configuration comes here; leave empty for default settings
+    },
+    ft = { "cs" }
+}
+
+return M
